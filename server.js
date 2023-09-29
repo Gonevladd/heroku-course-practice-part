87a6cb4c9a90415aef3bb68497b05b25f77a6ccf
Vladyslav <voicehovskiy.vlad@gmail.com>
2023-09-29 16:31:24 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+/**
+ * Created by vladyslavviotsekhovskyi on 29.09.2023.
+ */
+
+const dotenv = require('dotenv');
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const HOST = process.env.HOST || 'localhost';
+const PORT = process.env.PORT || 3002;
+
+app.get('/api/v1/endpoint', (req, res) => {
+    res.json({ success: true });
+});
+
+
+app.listen(PORT, () => console.log(`✅  API Server started: http://${HOST}:${PORT}/api/v1/endpoint`));
