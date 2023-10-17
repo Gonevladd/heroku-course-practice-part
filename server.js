83d2556e4c39214bf1a1427d6c9fd6162638efe1
Vladyslav <voicehovskiy.vlad@gmail.com>
2023-10-17 12:42:48 +0300

    added file support

--- server.js
+++ server.js
@@ -20,10 +20,21 @@ app.get('/api/accounts', async (req, res) => {
     res.json({ data: await AccountController.getAllAccounts() });
 });
 
-app.post('/api/accounts', async (req, res) => {
+app.post('/api/accounts', upload.single('file'), (req, res) => {
     console.log('file body');
-    console.log(req);
+    console.log(req.file);
     // res.json({ data: await AccountController.insertAccount(req.body) });
 });
 
 app.listen(PORT, () => console.log(`✅  API Server started: http://${HOST}:${PORT}/api/v1/endpoint`));
+
+let multer = require('multer');
+let storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, './../uploads');
+    },
+    filename: function (req, file, callback) {
+        callback(null, Date.now() + '-' + file.originalname);
+    }
+});
+let upload = multer({ storage: storage });
