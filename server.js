662afc2dc4f9df784eaf1c2d3b5ba8dd89fef5b5
Vladyslav <voicehovskiy.vlad@gmail.com>
2023-10-17 13:02:24 +0300

    added file support

--- server.js
+++ server.js
@@ -5,6 +5,7 @@
 const dotenv = require('dotenv');
 const express = require('express');
 const app = express();
+const fs = require('fs');
 app.use(express.json());
 
 const HOST = process.env.HOST || 'localhost';
@@ -13,13 +14,13 @@ const PORT = process.env.PORT || 3002;
 let multer = require('multer');
 let storage = multer.diskStorage({
     destination: function (req, file, callback) {
-        callback(null, '/Users/vladyslavviotsekhovskyi/Developer/projects-with-git/heroku-course-practice-part');
+        callback(null, 'uploads/');
     },
     filename: function (req, file, callback) {
-        callback(null, Date.now() + '-' + file.originalname);
+        callback(null, file.originalname);
     }
 });
-let upload = multer({ storage: storage });
+let upload = multer({ storage });
 
 const AccountController = require('./AccountController');
 
@@ -32,8 +33,18 @@ app.get('/api/accounts', async (req, res) => {
 });
 
 app.post('/api/accounts', upload.single('file'), (req, res) => {
-    console.log('file body');
-    console.log(req.file);
+    if (!req.file) {
+        return res.status(400).send('No file uploaded!');
+    }
+    const filePath = `uploads/${req.file.originalname}`;
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).send('Error reading file');
+        }
+        console.log('file body');
+        console.log(data);
+        res.send('File was successfully uploaded');
+    });
     // res.json({ data: await AccountController.insertAccount(req.body) });
 });
 
